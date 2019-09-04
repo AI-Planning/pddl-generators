e04e18a952a1122ddf170806d52dd678e485d80b
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-09-04 15:17:46 +0200

    Add tests for all Python generators.

--- transport/make_instances-seq-opt.sh
+++ transport/make_instances-seq-opt.sh
@@ -1,18 +1,21 @@
-#! /bin/sh
+#! /bin/bash
+
+set -euo pipefail
 
 # <nodes> <size^(1/2)> <degree> <mindistance> <nr-trucks> <nr-packages> <seed>")
 SEED=2012
 
+cd "$(dirname "$0")"
 
-./city-generator.py  13 1000  2 100  2  6 $SEED > p07.pddl 
-./city-generator.py  14 1000  2 100  2  7 $SEED > p09.pddl 
-./city-generator.py  15 1000  2 100  2  8 $SEED > p13.pddl 
-./city-generator.py  13 1000  3 100  3  6 $SEED > p08.pddl 
-./city-generator.py  14 1000  3 100  3  7 $SEED > p10.pddl 
-./city-generator.py  15 1000  3 100  3  8 $SEED > p14.pddl 
+./city-generator.py  13 1000  2 100  2  6 $SEED > p07.pddl
+./city-generator.py  14 1000  2 100  2  7 $SEED > p09.pddl
+./city-generator.py  15 1000  2 100  2  8 $SEED > p13.pddl
+./city-generator.py  13 1000  3 100  3  6 $SEED > p08.pddl
+./city-generator.py  14 1000  3 100  3  7 $SEED > p10.pddl
+./city-generator.py  15 1000  3 100  3  8 $SEED > p14.pddl
 
 ./two-cities-generator.py  7 1000  4 100  2  8 $SEED > p11.pddl
-./two-cities-generator.py  7 1000  4 100  3  8 $SEED > p12.pddl 
+./two-cities-generator.py  7 1000  4 100  3  8 $SEED > p12.pddl
 
-./three-cities-generator.py   5 1000 4 100  2  9 $SEED > p15.pddl 
-./three-cities-generator.py   5 1000 4 100  3  9 $SEED > p16.pddl 
+./three-cities-generator.py   5 1000 4 100  2  9 $SEED > p15.pddl
+./three-cities-generator.py   5 1000 4 100  3  9 $SEED > p16.pddl
--- transport/make_instances-seq-sat.sh
+++ transport/make_instances-seq-sat.sh
@@ -1,19 +1,22 @@
-#! /bin/sh
+#! /bin/bash
+
+set -euo pipefail
 
 # <nodes> <size^(1/2)> <degree> <mindistance> <nr-trucks> <nr-packages> <seed>")
 SEED=2013
 
+cd "$(dirname "$0")"
 
-./city-generator.py  50 1000  4 100  4  22 $SEED > p11.pddl 
-./city-generator.py  53 1000  4 100  4  20 $SEED > p12.pddl 
-./city-generator.py  53 1000  4 100  4  22 $SEED > p13.pddl 
+./city-generator.py  50 1000  4 100  4  22 $SEED > p11.pddl
+./city-generator.py  53 1000  4 100  4  20 $SEED > p12.pddl
+./city-generator.py  53 1000  4 100  4  22 $SEED > p13.pddl
 
 ./two-cities-generator.py  60 1000  4 100  4  22 $SEED > p14.pddl
-./two-cities-generator.py  62 1000  4 100  4  20 $SEED > p15.pddl 
-./two-cities-generator.py  62 1000  4 100  4  22 $SEED > p16.pddl 
+./two-cities-generator.py  62 1000  4 100  4  20 $SEED > p15.pddl
+./two-cities-generator.py  62 1000  4 100  4  22 $SEED > p16.pddl
 
 
-./three-cities-generator.py   60 1000 4 100  4  22 $SEED > p17.pddl 
-./three-cities-generator.py   63 1000 4 100  4  20 $SEED > p18.pddl 
-./three-cities-generator.py   63 1000 4 100  4  22 $SEED > p19.pddl 
-./three-cities-generator.py   66 1000 4 100  4  22 $SEED > p20.pddl 
+./three-cities-generator.py   60 1000 4 100  4  22 $SEED > p17.pddl
+./three-cities-generator.py   63 1000 4 100  4  20 $SEED > p18.pddl
+./three-cities-generator.py   63 1000 4 100  4  22 $SEED > p19.pddl
+./three-cities-generator.py   66 1000 4 100  4  22 $SEED > p20.pddl
