7451b12a03e7be8008aebc0ee0d779976a5178ef
Jendrik Seipp <jendrik.seipp@liu.se>
2021-11-03 11:18:44 +0100

    Use argparse to parse Spanner arguments.

--- spanner/spanner-generator.py
+++ spanner/spanner-generator.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python
-import sys
+
+import argparse
 import random
 
-# *****************#
-# Functions declarations
-# *****************#
 
-
-def get_objects():
+def get_objects(args):
     str_objects = "\n"
 
     # -- man
-    str_objects = str_objects + "     bob - man\n"
+    str_objects = str_objects + "     bob - man\n    "
 
     # -- spanner
-    for i in range(1, num_spanners + 1):
+    for i in range(1, args.spanners + 1):
         str_objects = str_objects + " spanner" + str(i)
     str_objects = str_objects + " - spanner\n    "
 
     # -- nuts
-    for i in range(1, num_nuts + 1):
+    for i in range(1, args.nuts + 1):
         str_objects = str_objects + " nut" + str(i)
     str_objects = str_objects + " - nut\n    "
 
     # -- locations
-    for i in range(1, num_locations + 1):
+    for i in range(1, args.locations + 1):
         str_objects = str_objects + " location" + str(i)
     str_objects = str_objects + " - location\n    "
 
@@ -33,30 +30,29 @@ def get_objects():
     return str_objects
 
 
-# *****************#
-def get_init():
+def get_init(args):
     str_init = "\n"
     str_init = str_init + "    (at bob shed)\n"
 
-    for i in range(1, num_spanners + 1):
+    for i in range(1, args.spanners + 1):
         str_init = (
             str_init
             + "    (at spanner"
             + str(i)
             + " location"
-            + str(random.randint(1, num_locations))
+            + str(random.randint(1, args.locations))
             + ")\n"
         )
         str_init = str_init + "    (useable spanner" + str(i) + ")\n"
 
-    for i in range(1, num_nuts + 1):
+    for i in range(1, args.nuts + 1):
         str_init = str_init + "    (loose nut" + str(i) + ")\n"
         str_init = str_init + "    (at nut" + str(i) + " gate)\n"
 
     str_init = str_init + "    (link shed location1)\n"
-    str_init = str_init + "    (link location" + str(num_locations) + " gate)\n"
+    str_init = str_init + "    (link location" + str(args.locations) + " gate)\n"
 
-    for i in range(1, num_locations):
+    for i in range(1, args.locations):
         str_init = (
             str_init + "    (link location" + str(i) + " location" + str(i + 1) + ")\n"
         )
@@ -64,45 +60,36 @@ def get_init():
     return str_init
 
 
-# *****************#
-
-
-def get_goals():
+def get_goals(args):
     str_goal = ""
     str_goal = str_goal + "\n  (and\n"
 
-    for i in range(1, num_nuts + 1):
+    for i in range(1, args.nuts + 1):
         str_goal = str_goal + "   (tightened nut" + str(i) + ")\n"
 
     str_goal = str_goal + ")"
     return str_goal
 
 
-# *****************#
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("spanners", type=int, help="number of spanners (min 1)")
+    parser.add_argument("nuts", type=int, help="number of nuts (min 1 and <= spanners)")
+    parser.add_argument("locations", type=int, help="number of locations (min 1)")
+    parser.add_argument("--problem-name", default="prob", help="number of locations (min 1)")
+    return parser.parse_args()
 
 
-# *****************#
-# MAIN
-# *****************#
-# Reading the command line arguments
-try:
-    num_spanners = int(sys.argv[1])
-    num_nuts = int(sys.argv[2])
-    num_locations = int(sys.argv[3])
-    name = "prob"
-except BaseException:
-    print("Usage: " + sys.argv[0] + " <num_spanners> <num_nuts> <num_locations>")
-    print("  num_spanners (min 1)")
-    print("  num_nuts (min 1 and <= num_spanners)")
-    print("  num_locations (min 1)")
+def main():
+    args = parse_args()
 
-    sys.exit(1)
+    random.seed()
 
-random.seed()
+    print("(define (problem " + args.problem_name + ")")
+    print(" (:domain spanner)")
+    print(" (:objects " + get_objects(args) + ")")
+    print(" (:init " + get_init(args) + ")")
+    print(" (:goal" + get_goals(args) + "))")
 
-print("(define (problem " + name + ")")
-print(" (:domain spanner)")
-print(" (:objects " + get_objects() + ")")
-print(" (:init " + get_init() + ")")
-print(" (:goal" + get_goals() + "))")
-sys.exit(0)
+
+main()
