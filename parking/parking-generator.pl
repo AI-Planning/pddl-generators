a7b5e0bea104002ba100fc8c181dc33335721ef5
Masataro Asai <guicho2.71828@gmail.com>
2021-11-25 11:08:41 -0500

    [parking] turn all tabs to spaces

--- parking/parking-generator.pl
+++ parking/parking-generator.pl
@@ -39,11 +39,11 @@ local %goal_cfg = goal_config();
 print define();
 
 sub define {
-	my $s = "";
-	$s .= "(define ";
+  my $s = "";
+  $s .= "(define ";
   $s .= "  (problem parking)\n";
-	$s .= "  (:domain parking)\n";
-# 	$s .= "  (:requirements :typing)\n";
+  $s .= "  (:domain parking)\n";
+#   $s .= "  (:requirements :typing)\n";
   $s .= objects();
   $s .= init();
   $s .= goal();
@@ -67,10 +67,10 @@ sub define {
 }
 
 sub define_untyped {
-	my $s = "";
-	$s .= "(define ";
+  my $s = "";
+  $s .= "(define ";
   $s .= "  (problem parking)\n";
-	$s .= "  (:domain parking-untyped)\n";
+  $s .= "  (:domain parking-untyped)\n";
   $s .= objects_untyped();
   $s .= init_untyped();
   $s .= goal();
@@ -90,27 +90,27 @@ sub define_untyped {
 sub objects {
   my $s = "";
   $s .= "  (:objects\n    ";
-	for my $car (@cars) {
-	  $s .= " $car ";
-	}; $s .= "- car\n     ";
-	for my $curb (@curbs) {
-	  $s .= "$curb ";
-	}
+  for my $car (@cars) {
+    $s .= " $car ";
+  }; $s .= "- car\n     ";
+  for my $curb (@curbs) {
+    $s .= "$curb ";
+  }
   $s .= "- curb\n  )\n";
-	return $s;
+  return $s;
 }
 
 sub objects_untyped {
   my $s = "";
   $s .= "  (:objects\n    ";
-	for my $car (@cars) {
-	  $s .= " $car ";
-	}; $s .= "\n     ";
-	for my $curb (@curbs) {
-	  $s .= "$curb ";
-	}
+  for my $car (@cars) {
+    $s .= " $car ";
+  }; $s .= "\n     ";
+  for my $curb (@curbs) {
+    $s .= "$curb ";
+  }
   $s .= "\n  )\n";
-	return $s;
+  return $s;
 }
 
 
@@ -118,30 +118,30 @@ sub init {
   my $s = "  (:init\n";
 
   for $curb (@curbs) {
-		if (defined($init_cfg{$curb})) {
-		  my $at_curb = $init_cfg{$curb};
-			$s .= "    (at-curb $at_curb)\n";
-			$s .= "    (at-curb-num $at_curb $curb)\n";
-			if (defined($init_cfg{$at_curb})) {
-			  $car_behind = $init_cfg{$at_curb};
-				$s .= "    (behind-car $car_behind $at_curb)\n";
-				$s .= "    (car-clear $car_behind)\n";
-			}
-			else {
-				$s .= "    (car-clear $at_curb)\n";
-			}
-		}
-		else { # it's clear
-		  $s .= "    (curb-clear $curb)\n";
-		}
-	}
+    if (defined($init_cfg{$curb})) {
+      my $at_curb = $init_cfg{$curb};
+      $s .= "    (at-curb $at_curb)\n";
+      $s .= "    (at-curb-num $at_curb $curb)\n";
+      if (defined($init_cfg{$at_curb})) {
+        $car_behind = $init_cfg{$at_curb};
+        $s .= "    (behind-car $car_behind $at_curb)\n";
+        $s .= "    (car-clear $car_behind)\n";
+      }
+      else {
+        $s .= "    (car-clear $at_curb)\n";
+      }
+    }
+    else { # it's clear
+      $s .= "    (curb-clear $curb)\n";
+    }
+  }
 
-	for my $key (keys(%cfg)) {
-	  $s .= "$key ===> " . $cfg{$key} ."\n";
-	}
+  for my $key (keys(%cfg)) {
+    $s .= "$key ===> " . $cfg{$key} ."\n";
+  }
 
-	$s .= "  )\n";
-	return $s;
+  $s .= "  )\n";
+  return $s;
 }
 
 
@@ -149,66 +149,66 @@ sub init_untyped {
   my $s = "  (:init\n";
 
   for $car (@cars) {
-	  $s .= "    (car $car)\n";
-	}
+    $s .= "    (car $car)\n";
+  }
   for $curb (@curbs) {
           $s .= "    (curb $curb)\n";
-	}
+  }
   for $curb (@curbs) {
-		if (defined($init_cfg{$curb})) {
-		  my $at_curb = $init_cfg{$curb};
-			$s .= "    (at-curb $at_curb)\n";
-			$s .= "    (at-curb-num $at_curb $curb)\n";
-			if (defined($init_cfg{$at_curb})) {
-			  $car_behind = $init_cfg{$at_curb};
-				$s .= "    (behind-car $car_behind $at_curb)\n";
-				$s .= "    (car-clear $car_behind)\n";
-			}
-			else {
-				$s .= "    (car-clear $at_curb)\n";
-			}
-		}
-		else { # it's clear
-		  $s .= "    (curb-clear $curb)\n";
-		}
-	}
+    if (defined($init_cfg{$curb})) {
+      my $at_curb = $init_cfg{$curb};
+      $s .= "    (at-curb $at_curb)\n";
+      $s .= "    (at-curb-num $at_curb $curb)\n";
+      if (defined($init_cfg{$at_curb})) {
+        $car_behind = $init_cfg{$at_curb};
+        $s .= "    (behind-car $car_behind $at_curb)\n";
+        $s .= "    (car-clear $car_behind)\n";
+      }
+      else {
+        $s .= "    (car-clear $at_curb)\n";
+      }
+    }
+    else { # it's clear
+      $s .= "    (curb-clear $curb)\n";
+    }
+  }
 
-	for my $key (keys(%cfg)) {
-	  $s .= "$key ===> " . $cfg{$key} ."\n";
-	}
+  for my $key (keys(%cfg)) {
+    $s .= "$key ===> " . $cfg{$key} ."\n";
+  }
 
-	$s .= "  )\n";
-	return $s;
+  $s .= "  )\n";
+  return $s;
 }
 
 
 sub goal {
   my $s;
-	##my $num_cars_against_curb = $num
-	$s .= "  (:goal\n";
-	$s .= "    (and\n";
+  ##my $num_cars_against_curb = $num
+  $s .= "  (:goal\n";
+  $s .= "    (and\n";
   for $curb (@curbs) {
-		if (defined($goal_cfg{$curb})) {
-		  my $at_curb = $goal_cfg{$curb};
-			$s .= "      (at-curb-num $at_curb $curb)\n";
-			if (defined($goal_cfg{$at_curb})) {
-			  $car_behind = $goal_cfg{$at_curb};
-				$s .= "      (behind-car $car_behind $at_curb)\n";
-			}
-		}
-	}
+    if (defined($goal_cfg{$curb})) {
+      my $at_curb = $goal_cfg{$curb};
+      $s .= "      (at-curb-num $at_curb $curb)\n";
+      if (defined($goal_cfg{$at_curb})) {
+        $car_behind = $goal_cfg{$at_curb};
+        $s .= "      (behind-car $car_behind $at_curb)\n";
+      }
+    }
+  }
 
-	$s .= "    )\n";
-	$s .= "  )\n";
-	return $s;
+  $s .= "    )\n";
+  $s .= "  )\n";
+  return $s;
 }
 
 sub pad {           # add leading zeros
   my ($x, $len) = @_;
   while (length($x) < $len) {
-	  $x = "0$x";
-	}
-	return $x;
+    $x = "0$x";
+  }
+  return $x;
 }
 
 # any valid config will have at least as many cars against curbs
@@ -219,63 +219,63 @@ sub pad {           # add leading zeros
 sub goal_config {
   my $num_against_curb = ($#curbs > $#cars) ? $#cars+1 : $#curbs+1; # min
   my %against;
-	for my $i (0..$num_against_curb - 1) {
-	  $against{$curbs[$i]} = $cars[$i];
-	}
-	for $i ($num_against_curb .. $#cars) {
-	  $against{$cars[$i - $num_against_curb]} = $cars[$i];
-	}
-	return %against;
+  for my $i (0..$num_against_curb - 1) {
+    $against{$curbs[$i]} = $cars[$i];
+  }
+  for $i ($num_against_curb .. $#cars) {
+    $against{$cars[$i - $num_against_curb]} = $cars[$i];
+  }
+  return %against;
 
 }
 
 sub init_config {
-	use POSIX qw(ceil floor);
+  use POSIX qw(ceil floor);
   my $min_against_curb = ceil(($#cars+1)/2); #random between min and number of curbs
   my $max_against_curb = ($#curbs > $#cars) ? $#cars+1 : $#curbs+1; # min
 
-	my $num_against_curb = rand_between($min_against_curb, $max_against_curb);
-	#print "$min_against_curb <= $num_against_curb <= $max_against_curb\n";
+  my $num_against_curb = rand_between($min_against_curb, $max_against_curb);
+  #print "$min_against_curb <= $num_against_curb <= $max_against_curb\n";
 
-	my @shuffled_cars = shuffle(@cars);
+  my @shuffled_cars = shuffle(@cars);
   my %against;
-	for my $i (0..$num_against_curb - 1) {
-	  $against{$curbs[$i]} = $shuffled_cars[$i];
-	}
-	for $i ($num_against_curb .. $#cars) {
-	  $against{$shuffled_cars[$i - $num_against_curb]} = $shuffled_cars[$i];
-	}
-	return %against;
+  for my $i (0..$num_against_curb - 1) {
+    $against{$curbs[$i]} = $shuffled_cars[$i];
+  }
+  for $i ($num_against_curb .. $#cars) {
+    $against{$shuffled_cars[$i - $num_against_curb]} = $shuffled_cars[$i];
+  }
+  return %against;
 }
 
 sub shuffle {
   my (@a) = @_;
-	srand;
-	@new = ();
-	while (@a) {
-		push(@new, splice(@a, rand @a, 1));
-	}
+  srand;
+  @new = ();
+  while (@a) {
+    push(@new, splice(@a, rand @a, 1));
+  }
   return @new;
 }
 
 sub rand_between {  # inclusive
   my ($min, $max) = @_;
-	return $min if $min == $max;
+  return $min if $min == $max;
  ($min, $max) = ($max, $min)  if  $min > $max;
  return $min + int rand(1 + $max - $min);
 }
 
 sub visual_layout {
   my (%against) = @_;
-	my $s;
+  my $s;
   for $curb (@curbs) {
-	  $s .= ";  $curb: ";
-		if (defined($against{$curb})) {
-		  $s .= $against{$curb}." ";
-			$s .= (defined($against{$against{$curb}}) ? $against{$against{$curb}}." " : "");
-		}
-		$s .= "\n";
-	}
-	return $s;
+    $s .= ";  $curb: ";
+    if (defined($against{$curb})) {
+      $s .= $against{$curb}." ";
+      $s .= (defined($against{$against{$curb}}) ? $against{$against{$curb}}." " : "");
+    }
+    $s .= "\n";
+  }
+  return $s;
 }
 
