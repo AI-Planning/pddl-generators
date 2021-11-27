fe1c47a6e54db77f9f2d439845235ca3ffed0ecd
Masataro Asai <guicho2.71828@gmail.com>
2021-11-27 13:21:43 -0500

    [domain] grippers

---
+++ pddl_generators/grippers/__init__.py
@@ -0,0 +1,51 @@
+
+
+# Each module M must implement following attributes:
+#
+# 1. M.main : A main function which takes two argparser.Namespace instances, args and
+#    args2. args is largely irrelevant except args.seed , which was
+#    supplied by the command line argument.
+#
+# 2. M.parser : A parser , which parses the "rest" arguments that are specific to each domain.
+#
+# 3. M.domain_file : An absolute pathname to the domain pddl file, or None when the domain file is produced.
+
+
+
+
+
+
+
+
+import os.path
+import argparse
+from pddl_generators.main import run
+import pathlib
+
+def rel(path): return os.path.join(os.path.dirname(__file__), path)
+
+
+
+domain_file = rel("domain.pddl")
+
+
+parser = argparse.ArgumentParser(
+    formatter_class = argparse.RawDescriptionHelpFormatter,
+    description = pathlib.Path(rel("README")).read_text(),
+)
+
+
+parser.add_argument("balls", type=int, help="the number of balls")
+parser.add_argument("robots", type=int, help="the number of robots")
+parser.add_argument("grippers", type=int, help="the number of grippers")
+parser.add_argument("rooms", type=int, help="the number of rooms")
+
+
+def main(args : argparse.Namespace, args2 : argparse.Namespace):
+
+    with open(args.output, mode="w") as f:
+        run(args,list(map(str,["python3", rel("gripper.py"), "--seed", args.seed *args.rest])),
+            stdout=f)
+
+    pass
+
